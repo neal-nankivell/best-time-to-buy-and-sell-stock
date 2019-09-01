dcfbd23ebd8bf5873430411edb3986f6843c70f3
neal.nankivell <neal.nankivell@gmail.com>
2019-09-01 11:21:44 +0200

    Added failing test case for an empty collection being passed as input.

--- Answer.Test/SolutionTests.cs
+++ Answer.Test/SolutionTests.cs
@@ -7,6 +7,7 @@ namespace Tests
     {
         [TestCase(7, 1, 5, 3, 6, 4, ExpectedResult = 5)]
         [TestCase(7, 6, 4, 3, 1, ExpectedResult = 0)]
+        [TestCase(ExpectedResult = 0)]
         public int MaxProfit(params int[] prices)
         {
             var sut = new Solution();
